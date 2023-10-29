80aff9c03067fd1d69752f3895cb5e38d7d1c070
Jinjun Liang <kumkee@users.noreply.github.com>
2023-10-29 16:07:20 +1300

    replace WebClient with HttpClient

--- download.fsx
+++ download.fsx
@@ -1,6 +1,5 @@
-open System.Net
+open System.Net.Http
 open System.IO
-open System
 
 let urlList =
     [ "https://gist.githubusercontent.com/DeMarko/6142417/raw/1cd301a5917141524b712f92c2e955e86a1add19/sample.ics"
@@ -10,14 +9,14 @@ let urlList =
       "https://portal.macleans.school.nz/index.php/ics/school.ics" ]
 
 
-let fetch url =
-    let req = WebRequest.Create(Uri(url))
-    let resp = req.GetResponse()
-    use stream = resp.GetResponseStream()
-    use reader = new StreamReader(stream)
-    let ret = reader.ReadToEnd()
-    printfn "%s" ret
-    printfn "%d" ret.Length
+let getAsync (client: HttpClient) (url: string) =
+    async {
+        let! resp = client.GetAsync url |> Async.AwaitTask
+        resp.EnsureSuccessStatusCode() |> ignore
+        let! ret = resp.Content.ReadAsStringAsync() |> Async.AwaitTask
+        printfn "%s" ret
+    }
 
 // urlList |> List.map fetch
-fetch urlList[4]
+let client = new HttpClient()
+getAsync client urlList[4] |> Async.RunSynchronously
